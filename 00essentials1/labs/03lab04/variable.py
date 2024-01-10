a1857a9a24e770631938dcc569023b9a84078307
AshBC91 <ashley.barb91@gmail.com>
2024-01-10 00:22:26 -0500

    add variable.py README.md in lab4

---
+++ 00essentials1/labs/03lab04/variable.py
@@ -0,0 +1,16 @@
+john = 3
+mary = 5
+adam = 6
+total_apples = john + mary + adam
+
+print("John's apples: ", john, ", Mary's apples: ", mary, ", Adam's apples: ", adam, sep="")
+print("Total number of apples:", total_apples)
+print()
+
+dom = 3
+tom = 11
+mom = 20
+total_oranges = (((adam // john) * (dom - tom) ** mom) % tom + mary) * 4 + 6
+
+print("Dom's oranges: ", dom, ", Tom's oranges: ", tom, ", Mom's oranges: ", mom, sep="")
+print("Total number of Oranges:", total_oranges)
