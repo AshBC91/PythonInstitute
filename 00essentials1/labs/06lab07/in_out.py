165672c461bdfdb1414aa7585011e71abb376efc
AshBC91 <ashley.barb91@gmail.com>
2024-06-19 14:39:46 -0400

    add in_out.py README.md in lab7

---
+++ 00essentials1/labs/06lab07/in_out.py
@@ -0,0 +1,10 @@
+a = float(input("a: "))
+b = float(input("b: "))
+
+print(a, "+", b, "=", a+b)
+print(a, "-", b, "=", a-b)
+print(a, "*", b, "=", a*b)
+print(a, "/", b, "=", a/b)
+
+print("\nThat's all, folks!")
+
